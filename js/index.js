ba84352f8f613620af8d40a61ae9cb55e3aef871
Tsar Andriy <tsarandrij@gmail.com>
2018-07-30 16:13:47 +0000

    Initial

---
+++ js/index.js
@@ -0,0 +1,28 @@
+// const hello = 'Hello World';
+// alert(hello);
+const userName = prompt('Enter your name');
+alert(userName);
+const greetings ='Hello, ' + userName;
+alert(greetings);
+const userAge = parseInt(prompt('Enter your age'));
+alert('Your age is: ' + userAge);
+
+const nextAge = userAge + 1;
+alert('Next year you will be: ' + nextAge);
+
+if (userAge >= 18) {
+    alert('You are adult');
+} else {
+    alert('You are child');
+}
+
+function add(a, b, c) {
+    const result = a + b + c;
+    return result;
+}
+
+const result = add(2, 2, 5);
+alert(result);
+
+
+
